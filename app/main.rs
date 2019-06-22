5c9b91e6b985457b150ee27432a142556d077c2d
Masaki Hara <ackie.h.gmai@gmail.com>
2019-06-22 19:06:20 +0900

    Use structopt to dispatch subcommands.

--- app/main.rs
+++ app/main.rs
@@ -1,5 +1,7 @@
 #![feature(async_await)]
 
+use structopt::StructOpt;
+
 use futures::compat::{Compat, Future01CompatExt};
 use futures::{FutureExt, TryFutureExt, TryStreamExt};
 use hyper::service::service_fn;
@@ -8,16 +10,39 @@ use runtime::net::TcpListener;
 
 mod routes;
 
+#[derive(Debug, Clone, StructOpt)]
+struct CommandOpt {
+    #[structopt(subcommand)]
+    subcommand: SubcommandOpt,
+}
+
+#[derive(Debug, Clone, StructOpt)]
+enum SubcommandOpt {
+    #[structopt(name = "server")]
+    ServerCommandOpt(ServerCommandOpt),
+}
+
 #[runtime::main]
 async fn main() -> failure::Fallible<()> {
-    server().await?;
+    let opt = CommandOpt::from_args();
+    match opt.subcommand {
+        SubcommandOpt::ServerCommandOpt(ref server_opt) => {
+            server(server_opt).await?;
+        }
+    }
     Ok(())
 }
 
-pub async fn server() -> failure::Fallible<()> {
+#[derive(Debug, Clone, StructOpt)]
+pub(crate) struct ServerCommandOpt {
+    #[structopt(short = "p", help = "on which port to listen")]
+    port: Option<u16>,
+}
+
+pub(crate) async fn server(opt: &ServerCommandOpt) -> failure::Fallible<()> {
     let new_svc = || service_fn(|req| crate::routes::route(req).boxed().compat());
 
-    let mut listener = TcpListener::bind("127.0.0.1:3000")?;
+    let mut listener = TcpListener::bind(("127.0.0.1", opt.port.unwrap_or(3000)))?;
     println!("Listening on {}", listener.local_addr()?);
 
     let incoming = listener.incoming().map_ok(Compat::new).compat();
