8c287db645bf3a9d3377d9f5ef4f434d1f911a2c
Masaki Hara <ackie.h.gmai@gmail.com>
2019-06-29 16:47:38 +0900

    Prototype router.

--- app/routes.rs
+++ app/routes.rs
@@ -1,18 +1,41 @@
-use hyper::{Body, Request, Response, StatusCode};
+use hyper::{Body, Method, Request, Response, StatusCode};
+
+use nails::{FromRequest, Routable, Router};
 
 pub(crate) async fn route(req: Request<Body>) -> failure::Fallible<Response<Body>> {
-    let path = req.uri().path();
-    let resp = if path == "/" {
-        index(req).await?
+    let router = {
+        let mut router = Router::new();
+        router.add_function_route(index);
+        router
+    };
+
+    let resp = if router.match_path(req.method(), req.uri().path()) {
+        router.respond(req)
     } else {
         Response::builder()
             .status(StatusCode::NOT_FOUND)
-            .body(Body::from(""))
+            .body(Body::from("Not Found"))
             .unwrap()
     };
     Ok(resp)
 }
 
-async fn index(_req: Request<Body>) -> failure::Fallible<Response<Body>> {
-    Ok(Response::new(Body::from("Hello, world!")))
+#[derive(Debug)]
+struct IndexRequest {}
+
+impl FromRequest for IndexRequest {
+    fn path_prefix_hint() -> &'static str {
+        "/"
+    }
+    fn match_path(method: &Method, path: &str) -> bool {
+        (*method == Method::GET || *method == Method::POST) && path == "/"
+    }
+
+    fn from_request(_req: Request<Body>) -> Self {
+        IndexRequest {}
+    }
+}
+
+fn index(_req: IndexRequest) -> Response<Body> {
+    Response::new(Body::from("Hello, world!"))
 }
