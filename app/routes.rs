02ede3aa507d48fb3d8a032d03a29a1fdda3a9ce
Masaki Hara <ackie.h.gmai@gmail.com>
2019-07-01 23:17:35 +0900

    Add more example route.

--- app/routes.rs
+++ app/routes.rs
@@ -1,4 +1,6 @@
 use hyper::{Body, Method, Request, Response, StatusCode};
+use serde::Serialize;
+use serde_derive::Serialize;
 
 use nails::{FromRequest, Routable, Router};
 
@@ -6,6 +8,7 @@ pub(crate) async fn route(req: Request<Body>) -> failure::Fallible<Response<Body
     let router = {
         let mut router = Router::new();
         router.add_function_route(index);
+        router.add_function_route(get_post);
         router
     };
 
@@ -29,4 +32,27 @@ struct IndexRequest {
 
 async fn index(req: IndexRequest) -> Response<Body> {
     Response::new(Body::from(format!("Hello, world! {:?}", req.a)))
+}
+
+#[derive(Debug, FromRequest)]
+#[nails(path = "/api/posts/1")]
+struct GetPostRequest {}
+
+#[derive(Debug, Serialize)]
+struct GetPostBody {
+    post: Post,
+}
+
+#[derive(Debug, Serialize)]
+struct Post {
+    body: String,
+}
+
+async fn get_post(_req: GetPostRequest) -> Response<Body> {
+    let body = GetPostBody {
+        post: Post {
+            body: String::from("foo"),
+        }
+    };
+    Response::new(Body::from(serde_json::to_string(&body).unwrap()))
 }
