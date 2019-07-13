7913399f1b2908157c87e202d719cb5f27a8c27b
Masaki Hara <ackie.h.gmai@gmail.com>
2019-07-13 18:37:43 +0900

    Workaround incompatibility between cargo-tarpaulin and trybuild.

--- lib/nails_derive/tests/ui.rs
+++ lib/nails_derive/tests/ui.rs
@@ -1,5 +1,9 @@
 #[test]
 fn ui() {
+    if option_env!("CARGO").unwrap_or("cargo").ends_with("cargo-tarpaulin") {
+        eprintln!("Skipping ui tests to avoid incompatibility between cargo-tarpaulin and trybuild");
+        return;
+    }
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/*.rs");
 }
