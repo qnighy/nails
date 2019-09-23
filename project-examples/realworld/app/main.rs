3dbb38c1f13feb8810a972108b38be6a2aac3ffa
Masaki Hara <ackie.h.gmai@gmail.com>
2019-09-23 15:13:19 +0900

    Test server by launching it in-process.

--- project-examples/realworld/app/main.rs
+++ project-examples/realworld/app/main.rs
@@ -1,10 +1,9 @@
 #[macro_use]
 extern crate diesel;
 
-use structopt::StructOpt;
-
 use hyper::Server;
 use nails::utils::hyper_ext::ServerBindExt;
+use structopt::StructOpt;
 
 use crate::context::AppCtx;
 
@@ -58,4 +57,83 @@ pub(crate) async fn server(ctx: &AppCtx, opt: &ServerCommandOpt) -> failure::Fal
     server.await?;
 
     Ok(())
+}
+
+#[cfg(test)]
+#[derive(Debug)]
+pub(crate) struct TestServer {
+    addr: std::net::SocketAddr,
+    shutdown: Option<futures::channel::oneshot::Sender<()>>,
+}
+
+#[cfg(test)]
+impl TestServer {
+    pub(crate) fn addr(&self) -> std::net::SocketAddr {
+        self.addr
+    }
+}
+
+#[cfg(test)]
+impl Drop for TestServer {
+    fn drop(&mut self) {
+        if let Some(shutdown) = self.shutdown.take() {
+            shutdown.send(()).ok();
+        }
+    }
+}
+
+#[cfg(test)]
+async fn server_for_test(ctx: &AppCtx) -> failure::Fallible<TestServer> {
+    use futures::channel::oneshot;
+    use futures::prelude::*;
+
+    let svc = crate::routes::build_route(ctx);
+
+    let host: std::net::IpAddr = "127.0.0.1".parse().unwrap();
+    let port = 0;
+    let mut addr = (host, port).into();
+
+    let server = Server::bind2_mut(&mut addr).serve(svc.with_context(ctx));
+
+    let (tx, rx) = oneshot::channel();
+
+    runtime::spawn(async move {
+        match server.with_graceful_shutdown(rx.map(|_| ())).await {
+            Ok(()) => {}
+            Err(e) => {
+                // TODO: log it properly
+                eprintln!("Server error: {}", e);
+            }
+        }
+    });
+
+    Ok(TestServer {
+        addr,
+        shutdown: Some(tx),
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use surf::http::StatusCode;
+
+    #[runtime::test]
+    async fn test_server() {
+        let server = init_test().await;
+
+        let url: String = format!("http://{}/", server.addr());
+        let mut res = surf::get(url).await.unwrap();
+        assert_eq!(res.status(), StatusCode::OK);
+        let body = res.body_string().await.unwrap();
+        assert_eq!(body, "Hello, world! []");
+        eprintln!("res = {:?}", res);
+    }
+
+    async fn init_test() -> TestServer {
+        dotenv::dotenv().ok();
+        let ctx = AppCtx::new();
+        server_for_test(&ctx).await.unwrap()
+    }
 }
